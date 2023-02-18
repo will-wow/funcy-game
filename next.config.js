e5cb6c93154660e84b4b3a20cfc83b75c99b8619
Will Ockelmann-Wagner <will@illust.space>
2023-02-18 00:49:52 -0800

    fix warnings about typescript

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    // Don't parse typescript.js as it's already been transpiled
+    // This silences a `Critical dependency: the request of a dependency is an expression` warning.
+    config.module.noParse ??= [];
+    config.module.noParse.push(
+      require.resolve("@ts-morph/common/dist/typescript.js")
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
